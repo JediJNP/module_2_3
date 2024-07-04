8e9a96aa34d5fca84fb46c3b79e131726f31d002
JediJNP <157392595+JediJNP@users.noreply.github.com>
2024-07-04 14:03:10 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,9 @@
+print('Задача "Нули ничто, отрицание недопустимо!"')
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+current_item = 0
+while current_item <= len(my_list):
+    if my_list[current_item] > 0:
+        print(my_list[current_item])
+    current_item = current_item + 1
+    if my_list[current_item] < 0:
+        break
